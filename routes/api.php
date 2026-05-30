4f5f393915c732ef4f683a0f4832e8ed1c46cf73
Alexandro Yule <alexandro.yule@caex.com.co>
2026-05-30 07:56:39 -0500

    fiix duplicate route names

--- routes/api.php
+++ routes/api.php
@@ -2,7 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::prefix('v1')->group(function () {
+Route::prefix('v1')->name('api.')->group(function () {
     require base_path('app/Modules/Auth/Routes/api.php');
     require base_path('app/Modules/Products/Routes/api.php');
     require base_path('app/Modules/Clients/Routes/api.php');
