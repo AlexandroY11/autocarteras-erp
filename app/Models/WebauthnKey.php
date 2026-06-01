8b2bd08f942cef8bf162bfaccba8b38e9f7bcc0f
Alexandro Yule <alexandroyule@gmail.com>
2026-06-01 11:32:24 -0500

    feat: Implement WebauthnKey model and bind it in AppServiceProvider for enhanced authentication

---
+++ app/Models/WebauthnKey.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Models;
+
+use LaravelWebauthn\Models\WebauthnKey as BaseWebauthnKey;
+
+class WebauthnKey extends BaseWebauthnKey
+{
+    // Heredamos todo de la librería
+}
